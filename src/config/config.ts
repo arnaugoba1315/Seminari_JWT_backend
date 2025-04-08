f8c6e28a9477390f1b01c6dde96f527b9e6c3eb2
arnaugoba1315 <arnau.gonzalez.barrios@estudiantat.upc.edu>
2025-04-08 11:25:12 +0200

    areglaoooo

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ export const TEST = process.env.NODE_ENV === 'test';
 
 export const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 
-export const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 12345
+export const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 9000
 
 export const SERVER = {
     SERVER_HOSTNAME,
